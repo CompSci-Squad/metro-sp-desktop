2a00cbc144558ad759e0d613cc374c509f6b4291
GuiPereira04 <gcfpereira04@gmail.com>
2024-11-29 18:10:57 -0300

    removendo-funcoes-totem

--- lib/src/telas/telaCadastrarNovoUsuario.dart
+++ lib/src/telas/telaCadastrarNovoUsuario.dart
@@ -78,23 +78,6 @@ class telaCadastrarNovoUsuario extends StatelessWidget {
           _buildDrawerItem('Verificar Cadastro Usuário', () {
             Navigator.pushReplacementNamed(context, '/telaVerificarCadastro');
           }),
-          _buildDrawerItem('Verificar Status Do Totem', () {
-            Navigator.pushReplacementNamed(context, '/telaVerificarStatusTotem');
-          }),
-          _buildDrawerItem(
-            'Reportar Falha De Totem',
-            () {
-              Navigator.pushReplacementNamed(context, '/telaReportarFalhaTotem');
-            },
-            textColor: Colors.red,
-          ),
-          _buildDrawerItem(
-            'Desbloquear Totem',
-            () {
-              Navigator.pushReplacementNamed(context, '/telaDesbloquearTotem');
-            },
-            textColor: Colors.green,
-          ),
         ],
       ),
     );
--- lib/src/telas/telaVerificarCadastro.dart
+++ lib/src/telas/telaVerificarCadastro.dart
@@ -61,23 +61,6 @@ class telaVerificarCadastro extends StatelessWidget {
           _buildDrawerItem('Verificar Cadastro Usuário', () {
             Navigator.pushReplacementNamed(context, '/telaVerificarCadastro');
           }),
-          _buildDrawerItem('Verificar Status Do Totem', () {
-            Navigator.pushReplacementNamed(context, '/telaVerificarStatusTotem');
-          }),
-          _buildDrawerItem(
-            'Reportar Falha De Totem',
-            () {
-              Navigator.pushReplacementNamed(context, '/telaReportarFalhaTotem');
-            },
-            textColor: Colors.red,
-          ),
-          _buildDrawerItem(
-            'Desbloquear Totem',
-            () {
-              Navigator.pushReplacementNamed(context, '/telaDesbloquearTotem');
-            },
-            textColor: Colors.green,
-          ),
         ],
       ),
     );
